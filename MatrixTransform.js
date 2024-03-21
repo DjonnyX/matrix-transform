a53115a3657505c6c2abbd9003a158a627c5ff06
djonnyx <djonnyx@gmail.com>
2024-03-21 07:49:25 +0300

    feat: added stub's to circle methods

--- MatrixTransform.js
+++ MatrixTransform.js
@@ -14,6 +14,9 @@ const MatrixWrapMethods = {
     CIRCLE: 'circle',
 };
 
+/**
+ * Cicle method not implemented
+ */
 class MatrixTransform {
     static clone(m) {
         const result = [], ml = m.length;
@@ -31,19 +34,28 @@ class MatrixTransform {
      * @returns 
      */
     static extractEdgeBorder(m, method = MatrixWrapMethods.BOX) {
-        const nm = m, ml = m.length, result = [], lEdge = [], rEdge = [];
-        result.push(...(nm.splice(0, 1)).flat());
-        for (let i = 0, l1 = ml - 2; i < l1; i++) {
-            const row = nm[i];
-            lEdge.push(...row.splice(0, 1));
-            rEdge.push(...row.splice(row.length - 1, 1));
+        switch (method) {
+            case MatrixWrapMethods.CIRCLE: {
+                // etc
+                return [];
+            }
+            case MatrixWrapMethods.BOX:
+            default: {
+                const nm = m, ml = m.length, result = [], lEdge = [], rEdge = [];
+                result.push(...(nm.splice(0, 1)).flat());
+                for (let i = 0, l1 = ml - 2; i < l1; i++) {
+                    const row = nm[i];
+                    lEdge.push(...row.splice(0, 1));
+                    rEdge.push(...row.splice(row.length - 1, 1));
+                }
+                result.push(...rEdge);
+                if (nm.length > 0) {
+                    result.push(...nm.splice(nm.length - 1, 1).flat().reverse());
+                }
+                result.push(...lEdge.reverse());
+                return result;
+            }
         }
-        result.push(...rEdge);
-        if (nm.length > 0) {
-            result.push(...nm.splice(nm.length - 1, 1).flat().reverse());
-        }
-        result.push(...lEdge.reverse());
-        return result;
     }
 
     /**
@@ -55,11 +67,20 @@ class MatrixTransform {
         // new deep immutable matrix
         const nm = MatrixTransform.clone(m), ml = m.length;
 
-        const cm = Math.ceil(ml * .5), result = [];
-        for (let i = 0; i < cm; i++) {
-            result.push(MatrixTransform.extractEdgeBorder(nm));
+        switch (method) {
+            case MatrixWrapMethods.CIRCLE: {
+                // etc
+                return [];
+            }
+            case MatrixWrapMethods.BOX:
+            default: {
+                const cm = Math.ceil(ml * .5), result = [];
+                for (let i = 0; i < cm; i++) {
+                    result.push(MatrixTransform.extractEdgeBorder(nm));
+                }
+                return result;
+            }
         }
-        return result;
     };
 
     /**
@@ -68,13 +89,23 @@ class MatrixTransform {
      * @returns 
      */
     static getEdgeBorderLengthFromMatrix(m, method = MatrixWrapMethods.BOX) {
-        if (m.length === 0) {
-            return 0;
+
+        switch (method) {
+            case MatrixWrapMethods.CIRCLE: {
+                // etc
+                return 0;
+            }
+            case MatrixWrapMethods.BOX:
+            default: {
+                if (m.length === 0) {
+                    return 0;
+                }
+                if (m.length === 1) {
+                    return 1;
+                }
+                return m.length * 4 - 4;
+            }
         }
-        if (m.length === 1) {
-            return 1;
-        }
-        return m.length * 4 - 4;
     }
 
     /**
@@ -91,11 +122,21 @@ class MatrixTransform {
             return 0;
         }
 
-        if (eb.length < 12) {
-            return 1;
-        }
 
-        return Math.ceil(Math.floor((eb.length + 4) * .25) * .5);
+        switch (method) {
+            case MatrixWrapMethods.CIRCLE: {
+                // etc
+                return [];
+            }
+            case MatrixWrapMethods.BOX:
+            default: {
+                if (eb.length < 12) {
+                    return 1;
+                }
+
+                return Math.ceil(Math.floor((eb.length + 4) * .25) * .5);
+            }
+        }
     }
 
     /**
@@ -109,14 +150,23 @@ class MatrixTransform {
             throw new Error(MatrixTransformErrors.INVALID_MATRIX_TYPE);
         }
 
-        const isOdd = eb.length % 2 !== 0;
-        const newEdgeBorderNestingSizeFromMatrix = m.length > 0 ? ((m.length + 2) * 4 - (m.length > 2 ? 4 : Number(!isOdd) + 1)) : Number(!isOdd) + 1;
-        const newEdgeBorderNestingSize = MatrixTransform.getNestingSizeFromBorderLength(eb);
-        if (newEdgeBorderNestingSize > newEdgeBorderNestingSizeFromMatrix) {
-            console.warn(MatrixTransformErrors.INVALID_EDGE_BORDER_SIZE);
-        }
+        switch (method) {
+            case MatrixWrapMethods.CIRCLE: {
+                // etc
+                return false;
+            }
+            case MatrixWrapMethods.BOX:
+            default: {
+                const isOdd = eb.length % 2 !== 0;
+                const newEdgeBorderNestingSizeFromMatrix = m.length > 0 ? ((m.length + 2) * 4 - (m.length > 2 ? 4 : Number(!isOdd) + 1)) : Number(!isOdd) + 1;
+                const newEdgeBorderNestingSize = MatrixTransform.getNestingSizeFromBorderLength(eb, method);
+                if (newEdgeBorderNestingSize > newEdgeBorderNestingSizeFromMatrix) {
+                    console.warn(MatrixTransformErrors.INVALID_EDGE_BORDER_SIZE);
+                }
 
-        return eb.length >= newEdgeBorderNestingSizeFromMatrix;
+                return eb.length >= newEdgeBorderNestingSizeFromMatrix;
+            }
+        }
     }
 
     /**
@@ -129,7 +179,7 @@ class MatrixTransform {
             throw new Error(MatrixTransformErrors.INVALID_MATRIX_TYPE);
         }
 
-        return MatrixTransform.unwrapEdgeBorders(m);
+        return MatrixTransform.unwrapEdgeBorders(m, method);
     }
 
     /**
@@ -143,35 +193,45 @@ class MatrixTransform {
             throw new Error(MatrixTransformErrors.INVALID_MATRIX_TYPE);
         }
 
-        // if (!MatrixTransform.isValidEdgeBorder(m, eb)) {
+        // if (!MatrixTransform.isValidEdgeBorder(m, eb, method)) {
         //     throw new Error(MatrixTransformErrors.INVALID_EDGE_BORDER_SIZE);
         // }
 
-        const meb = [...eb], result = m, isNew = result.length === 0;
-        // Проверки на валидную длину нет
-        if (isNew) {
-            const isOdd = meb.length % 4 !== 0;
-            if (isOdd) {
-                result.push(meb);
+
+        switch (method) {
+            case MatrixWrapMethods.CIRCLE: {
+                // etc
+                return [];
+            }
+            case MatrixWrapMethods.BOX:
+            default: {
+                const meb = [...eb], result = m, isNew = result.length === 0;
+                // Проверки на валидную длину нет
+                if (isNew) {
+                    const isOdd = meb.length % 4 !== 0;
+                    if (isOdd) {
+                        result.push(meb);
+                        return result;
+                    }
+
+                    const newSize = 2, t = meb.splice(0, newSize), b = meb.splice(0, newSize).reverse();
+                    result.push(t, b);
+                    return result;
+                }
+                else {
+                    const prevSize = m.length, newSize = prevSize + 2, t = meb.splice(0, newSize), r = meb.splice(0, prevSize),
+                        b = meb.splice(0, newSize).reverse(), l = meb.splice(0, prevSize).reverse();
+                    result.splice(0, 0, t);
+                    result.push(b);
+                    for (let i = 0, len = prevSize; i < len; i++) {
+                        const j = i + 1;
+                        result[j].splice(0, 0, l[i]);
+                        result[j].push(r[i]);
+                    }
+                }
                 return result;
             }
-
-            const newSize = 2, t = meb.splice(0, newSize), b = meb.splice(0, newSize).reverse();
-            result.push(t, b);
-            return result;
         }
-        else {
-            const prevSize = m.length, newSize = prevSize + 2, t = meb.splice(0, newSize), r = meb.splice(0, prevSize),
-                b = meb.splice(0, newSize).reverse(), l = meb.splice(0, prevSize).reverse();
-            result.splice(0, 0, t);
-            result.push(b);
-            for (let i = 0, len = prevSize; i < len; i++) {
-                const j = i + 1;
-                result[j].splice(0, 0, l[i]);
-                result[j].push(r[i]);
-            }
-        }
-        return result;
     }
 
     /**
@@ -182,7 +242,7 @@ class MatrixTransform {
     static wrap(unwrapedMatrix, method = MatrixWrapMethods.BOX) {
         const result = [], um = unwrapedMatrix.reverse();
         for (let i = 0, l = um.length; i < l; i++) {
-            MatrixTransform.appendEdgeBorder(result, um[i]);
+            MatrixTransform.appendEdgeBorder(result, um[i], method);
         }
         return result;
     }
@@ -197,35 +257,49 @@ class MatrixTransform {
     /**
      * @param {Number} offset Default [0-1]
      * @param {keyof RotateMetricTypes} metrics RotateMetricTypes.PERCENTAGE is default. RotateMetricTypes.DEGREES in degrees
-     * @param {keyof MatrixWrapMethods} method
+     * @param {{method: keyof MatrixWrapMethods, antialiasing: Number}} options
      * @returns 
      */
-    rotate(offset, metrics, method = MatrixWrapMethods.BOX) {
-        const m = metrics === RotateMetricTypes.DEGREES ? offset / 360 : offset, unwrap = MatrixTransform.unwrap(this._matrix);
-        for (let i = 0, l = unwrap.length; i < l; i++) {
-            const seq = unwrap[i];
-            let dx = Math.round(m * seq.length);
-            const dir = Math.sign(dx);
-            while (dx) {
-                if (dir === 1) {
-                    const el = seq[seq.length - 1];
-                    seq.splice(seq.length - 1, 1);
-                    seq.splice(0, 0, el);
-                } else {
-                    const el = seq[0];
-                    seq.splice(0, 1);
-                    seq.push(el);
+    rotate(offset, metrics, options = { method: MatrixWrapMethods.BOX, antialiasing: 0 }) {
+        const m = metrics === RotateMetricTypes.DEGREES ? offset / 360 : offset, unwrap = MatrixTransform.unwrap(this._matrix, options.method);
+
+        switch (method) {
+            case MatrixWrapMethods.CIRCLE: {
+                // etc
+                return this._matrix;
+            }
+            case MatrixWrapMethods.BOX:
+            default: {
+                for (let i = 0, l = unwrap.length; i < l; i++) {
+                    const seq = unwrap[i];
+                    let dx = Math.round(m * seq.length);
+                    const dir = Math.sign(dx);
+                    while (dx) {
+                        if (dir === 1) {
+                            const el = seq[seq.length - 1];
+                            seq.splice(seq.length - 1, 1);
+                            seq.splice(0, 0, el);
+                        } else {
+                            const el = seq[0];
+                            seq.splice(0, 1);
+                            seq.push(el);
+                        }
+                        dx -= dir;
+                    }
                 }
-                dx -= dir;
+                const rotatedMatrix = MatrixTransform.wrap(unwrap);
+                this._matrix = rotatedMatrix;
+                return this._matrix;
             }
         }
-        const rotatedMatrix = MatrixTransform.wrap(unwrap);
-        this._matrix = rotatedMatrix;
-        return this._matrix;
     }
 
     toString() {
         return !!this._matrix ? this._matrix.toString() : undefined;
+    }
+
+    toArray() {
+        return MatrixTransform.clone(this._matrix);
     }
 }
 
@@ -233,4 +307,5 @@ module.exports = {
     MatrixTransformErrors,
     MatrixTransform,
     RotateMetricTypes,
+    MatrixWrapMethods,
 };
